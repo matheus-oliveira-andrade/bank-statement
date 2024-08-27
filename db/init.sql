a4270a5f468873edbbed8a81acd45d2f153115dd
Matheus de Oliveira <32457879+matheus-oliveira-andrade@users.noreply.github.com>
2024-08-27 20:41:39 -0300

    refactor: explicit database creation on init sql (#20)

--- db/init.sql
+++ db/init.sql
@@ -1,9 +1,13 @@
+CREATE DATABASE accountdb;
+
+\c accountdb
+
 CREATE TABLE IF NOT EXISTS accounts (
-  Id SERIAL PRIMARY KEY,
-  Number VARCHAR(15),
-  Name VARCHAR(120),
-  Document VARCHAR(14),
-  Balance BIGINT,
-  CreatedAt TIMESTAMP,
-  UpdatedAt TIMESTAMP
+   Id SERIAL PRIMARY KEY,
+   Number VARCHAR(15),
+   Name VARCHAR(120),
+   Document VARCHAR(14),
+   Balance BIGINT,
+   CreatedAt TIMESTAMP,
+   UpdatedAt TIMESTAMP
 );
